a3f7d27d314b1bb8bee2f371f869e29fea23da09
Matthijs Groen <matthijs.groen@gmail.com>
2010-10-24 11:25:44 +0200

    Blank Rails 3.0.1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealTimeStrategy::Application.config.secret_token = '2967a3218a0feeefcbcdab7c7e84278bcf5b700264d14db771d38bf0b4cd3ce36a0e7b2079fd803c5e7549a302a036fbae56f67e170b226805b14f4128a19aa4'
